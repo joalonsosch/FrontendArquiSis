2a4dec3db948f91b5a50fec157bbf32ebe2c7229
alexiisvs <89324558+alexiisvs@users.noreply.github.com>
2025-04-24 18:59:57 -0400

    fix: ajustes para funcionamiento de auth0

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import styles from './LandingPage.module.css';
 import image from '../assets/LandingPageGPT.png';
 import { useAuth0 } from '@auth0/auth0-react';
