fa2b41e0123d3060ebbf5999409c0859bf3ecc20
NICOLAS ACEVEDO <nicobk2003@gmail.com>
2025-04-28 16:29:09 -0400

    callApi: rollback

--- src/services/callApi.jsx
+++ src/services/callApi.jsx
@@ -1,17 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_BACKEND_DOMAIN || 'http://localhost:3000';
-const AUDIENCE = import.meta.env.VITE_AUTH0_AUDIENCE || 'https://api.nacevedom.me';
-
+const API_BASE = import.meta.env.VITE_BACKEND_DOMAIN;
 
 export function useApi() {
   const { getAccessTokenSilently } = useAuth0();
 
   async function callApi(options) {
-    const token = await getAccessTokenSilently({
-      audience: AUDIENCE,
-    });
+    const token = await getAccessTokenSilently();
     console.log('Token:', token);
     const response = await axios({
       baseURL: API_BASE,
