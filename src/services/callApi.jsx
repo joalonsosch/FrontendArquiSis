6164f5849a09f9a389e4709a3ed399258adaa2ef
NICOLAS ACEVEDO <nicobk2003@gmail.com>
2025-04-28 17:50:05 -0400

    callapi

--- src/services/callApi.jsx
+++ src/services/callApi.jsx
@@ -2,6 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_BACKEND_DOMAIN || 'http://localhost:3000';
+console.log('API_BASE:', API_BASE);
 
 export function useApi() {
   const { getAccessTokenSilently } = useAuth0();
