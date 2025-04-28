4afe41d2dc6abbceeba8559d69f56bc7d7fe1c3c
NICOLAS ACEVEDO <nicobk2003@gmail.com>
2025-04-28 17:36:01 -0400

    Rollback

--- src/services/callApi.jsx
+++ src/services/callApi.jsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
-const API_BASE = 'https://api.nacevedom.me';
+const API_BASE = import.meta.env.VITE_BACKEND_DOMAIN || 'http://localhost:3000';
 
 export function useApi() {
   const { getAccessTokenSilently } = useAuth0();
