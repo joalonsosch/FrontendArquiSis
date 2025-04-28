331475f7e3a4e1e4d099e65411c8e0df4123eb00
NICOLAS ACEVEDO <nicobk2003@gmail.com>
2025-04-28 17:25:24 -0400

    Try

--- src/services/callApi.jsx
+++ src/services/callApi.jsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_BACKEND_DOMAIN || 'http://localhost:3000';
+const API_BASE = 'https://api.nacevedom.me';
 
 export function useApi() {
   const { getAccessTokenSilently } = useAuth0();
