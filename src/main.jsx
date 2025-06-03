659487fabab31f11b9d6bb3edc129eb658ee74bd
NICOLAS ACEVEDO <nicobk2003@gmail.com>
2025-06-02 23:58:50 -0400

    feat: checkeo cloudfront

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
+console.log(domain, clientId, audience);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
